2f1e2fc41831a06e61240e06ca366aee88293eaf
Mert Aydar <mertaydar@outlook.com>
2017-12-06 23:22:02 +0300

    Gui dev.
    Polycolletion prob. solved.

--- GUI/mainFrame2.py
+++ GUI/mainFrame2.py
@@ -9,63 +9,100 @@
 import os
 import vlc
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib.collections import PolyCollection
 import librosa
 import matplotlib
-from numpy import arange, sin, pi
-matplotlib.use("Qt5Agg")
+import numpy as np
 from librosa import display
+from librosa import core
+from librosa import util
+from librosa.util.exceptions import ParameterError
+matplotlib.use("Qt5Agg")
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QVBoxLayout, QSizePolicy
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QVBoxLayout, QSizePolicy, QTableWidgetItem
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from PyQt5.QtCore import pyqtSlot
+
 class Ui_MainWindow(object):
+    files = []
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(432, 478)
+        MainWindow.resize(493, 658)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.widget = QtWidgets.QWidget(self.centralwidget)
-        self.widget.setGeometry(QtCore.QRect(0, 0, 341, 227))
-        self.widget.setObjectName("widget")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.widget)
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout_1 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_1.setObjectName("horizontalLayout_1")
+        self.songListWidget = QtWidgets.QListWidget(self.centralwidget)
+        self.songListWidget.setObjectName("songListWidget")
+        self.horizontalLayout_1.addWidget(self.songListWidget)
+        self.loadButton = QtWidgets.QPushButton(self.centralwidget)
+        self.loadButton.setObjectName("loadButton")
+        self.horizontalLayout_1.addWidget(self.loadButton)
+        self.verticalLayout.addLayout(self.horizontalLayout_1)
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.songListWidget = QtWidgets.QListWidget(self.widget)
-        self.songListWidget.setObjectName("songListWidget")
-        self.horizontalLayout_2.addWidget(self.songListWidget)
-        self.loadButton = QtWidgets.QPushButton(self.widget)
-        self.loadButton.setObjectName("loadButton")
-        self.loadButton.clicked.connect(self.openFileNamesDialog)
-        self.horizontalLayout_2.addWidget(self.loadButton)
-        self.verticalLayout.addLayout(self.horizontalLayout_2)
-        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
-        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
-        self.playSongButton = QtWidgets.QPushButton(self.widget)
+        self.playSongButton = QtWidgets.QPushButton(self.centralwidget)
         self.playSongButton.setObjectName("playSongButton")
-        self.playSongButton.clicked.connect(self.playSong)
-        self.horizontalLayout_3.addWidget(self.playSongButton)
-        self.pauseSongButton = QtWidgets.QPushButton(self.widget)
+        self.horizontalLayout_2.addWidget(self.playSongButton)
+        self.pauseSongButton = QtWidgets.QPushButton(self.centralwidget)
         self.pauseSongButton.setObjectName("pauseSongButton")
-        self.pauseSongButton.clicked.connect(self.pauseSong)
-        self.horizontalLayout_3.addWidget(self.pauseSongButton)
-        self.stopSongButton = QtWidgets.QPushButton(self.widget)
+        self.horizontalLayout_2.addWidget(self.pauseSongButton)
+        self.stopSongButton = QtWidgets.QPushButton(self.centralwidget)
         self.stopSongButton.setObjectName("stopSongButton")
-        self.stopSongButton.clicked.connect(self.stopSong)
-        self.horizontalLayout_3.addWidget(self.stopSongButton)
-        self.verticalLayout.addLayout(self.horizontalLayout_3)
+        self.horizontalLayout_2.addWidget(self.stopSongButton)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
+        self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName("tabWidget")
+        self.tab = QtWidgets.QWidget()
+        self.tab.setObjectName("tab")
+        self.gridLayout_3 = QtWidgets.QGridLayout(self.tab)
+        self.gridLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.tabLayout1 = QtWidgets.QGridLayout()
+        self.tabLayout1.setObjectName("tabLayout1")
+        self.tabLayout1Label = QtWidgets.QLabel(self.tab)
+        self.tabLayout1Label.setAlignment(QtCore.Qt.AlignCenter)
+        self.tabLayout1Label.setObjectName("tabLayout1Label")
+        self.tabLayout1.addWidget(self.tabLayout1Label, 0, 0, 1, 1)
+        self.tablLayout1TableWidget = QtWidgets.QTableWidget(self.tab)
+        self.tablLayout1TableWidget.setObjectName("tablLayout1TableWidget")
+        self.tablLayout1TableWidget.setColumnCount(0)
+        self.tablLayout1TableWidget.setRowCount(0)
+        self.tabLayout1.addWidget(self.tablLayout1TableWidget, 1, 0, 1, 1)
+        self.gridLayout_3.addLayout(self.tabLayout1, 0, 0, 1, 1)
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QtWidgets.QWidget()
+        self.tab_2.setObjectName("tab_2")
+        self.gridLayout_4 = QtWidgets.QGridLayout(self.tab_2)
+        self.gridLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout_4.setObjectName("gridLayout_4")
+        self.tabLayout2 = QtWidgets.QGridLayout()
+        self.tabLayout2.setObjectName("tabLayout2")
+        self.gridLayout_4.addLayout(self.tabLayout2, 0, 0, 1, 1)
+        self.tabWidget.addTab(self.tab_2, "")
+        self.gridLayout.addWidget(self.tabWidget, 1, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 432, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 493, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
         
-
+        self.stopSongButton.clicked.connect(self.stopSong)
+        self.pauseSongButton.clicked.connect(self.pauseSong)
+        self.playSongButton.clicked.connect(self.playSong)
+        self.loadButton.clicked.connect(self.openFileNamesDialog)
         self.retranslateUi(MainWindow)
+        self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
@@ -75,31 +112,45 @@ class Ui_MainWindow(object):
         self.playSongButton.setText(_translate("MainWindow", "Play"))
         self.pauseSongButton.setText(_translate("MainWindow", "Pause"))
         self.stopSongButton.setText(_translate("MainWindow", "Stop"))
+        self.tabLayout1Label.setText(_translate("MainWindow", "TextLabel"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Tab 1"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Tab 2"))
         
     def openFileNamesDialog(self): 
-        fileNames , _ = QtWidgets.QFileDialog.getOpenFileNames(None, 'Open File', os.getenv('HOME'))
+        fileNames , _ = QtWidgets.QFileDialog.getOpenFileNames(None, 'Open File', os.getenv('HOME'), "Musics (*.mp3 *.wav)")
         if fileNames:
-            print(fileNames)    
-            self.files = fileNames
 #            Just keep file names not path
             for i in range(0,len(fileNames)):
+                self.files.append(fileNames[i])
                 x = fileNames[i].split('/')
-                print(x)
                 self.songListWidget.addItems(x[-1:])
+            print(self.files)
             
     def playSong(self):
         if self.songListWidget.currentItem():
-            media = instance.media_new(self.songListWidget.currentItem().text())
+            media = instance.media_new(self.files[self.songListWidget.currentRow()])
             player.set_media(media)
             player.play()
-            sc = MyStaticMplCanvas(self.widget, width=5, height=4, dpi=100)
-            self.verticalLayout.addWidget(sc)
-#            y, sr = librosa.load(librosa.util.example_audio_file(), duration=10)
-#            x = plt.figure()
-#            plt.subplot(3, 1, 1)
-#            librosa.display.waveplot(y, sr=sr)
-#            plt.title('Monophonic')
-            self.verticalLayout.addWidget(sc)
+            sc = MyStaticMplCanvas(self.centralwidget, width=2, height=4, dpi=100, index=self.files[self.songListWidget.currentRow()])
+            self.tabLayout1.addWidget(sc)
+            _translate = QtCore.QCoreApplication.translate
+            self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", self.files[self.songListWidget.currentRow()].split('/')[-1:][0]))
+            self.tablLayout1TableWidget.setRowCount(4)
+            self.tablLayout1TableWidget.setColumnCount(2)
+            self.tablLayout1TableWidget.setItem(0,0, QTableWidgetItem("Cell (1,1)"))
+            self.tablLayout1TableWidget.setItem(0,1, QTableWidgetItem("Cell (1,2)"))
+            self.tablLayout1TableWidget.setItem(1,0, QTableWidgetItem("Cell (2,1)"))
+            self.tablLayout1TableWidget.setItem(1,1, QTableWidgetItem("Cell (2,2)"))
+            self.tablLayout1TableWidget.setItem(2,0, QTableWidgetItem("Cell (3,1)"))
+            self.tablLayout1TableWidget.setItem(2,1, QTableWidgetItem("Cell (3,2)"))
+            self.tablLayout1TableWidget.setItem(3,0, QTableWidgetItem("Cell (4,1)"))
+            self.tablLayout1TableWidget.setItem(3,1, QTableWidgetItem("Cell (4,2)"))
+#            # Adds new tab
+#            self.tab_3 = QtWidgets.QWidget()
+#            self.tab_3.setObjectName("tab_3")
+#            self.gridLayout_4 = QtWidgets.QGridLayout(self.tab_3)
+#            self.tabWidget.addTab(self.tab_3, "")
+            
             
     def pauseSong(self):
         if self.songListWidget.currentItem():
@@ -111,37 +162,79 @@ class Ui_MainWindow(object):
             
 class MyMplCanvas(FigureCanvas):
     """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100, index=2):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
-        # We want the axes cleared every time plot() is called
-        self.axes.hold(False)
+        self.compute_initial_figure(index)
 
-        self.compute_initial_figure()
-
-        #
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
 
         FigureCanvas.setSizePolicy(self,
-                QSizePolicy.Expanding,
-                QSizePolicy.Expanding)
+                                   QtWidgets.QSizePolicy.Expanding,
+                                   QtWidgets.QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
 
-    def compute_initial_figure(self):
+    def compute_initial_figure(self, index):
         pass
     
 class MyStaticMplCanvas(MyMplCanvas):
     """Simple canvas with a sine plot."""
-    def compute_initial_figure(self):
-        y, sr = librosa.load(librosa.util.example_audio_file(), duration=10)
-        x = plt.figure()
-        plt.subplot(3, 1, 1)
-        librosa.display.waveplot(y, sr=sr)
-        plt.title('Monophonic')
-        t = arange(0.0, 3.0, 0.01)
-        s = sin(2*pi*t)
-        
+
+    def compute_initial_figure(self, index):
+        y, sr = librosa.load(index, duration=10)
+        max_points=5e4
+        x_axis='time'
+        offset=0.0
+        max_sr=1000
+        util.valid_audio(y, mono=False)
+
+        if not (isinstance(max_sr, int) and max_sr > 0):
+            raise ParameterError('max_sr must be a non-negative integer')
+    
+        target_sr = sr
+        hop_length = 1
+    
+        if max_points is not None:
+            if max_points <= 0:
+                raise ParameterError('max_points must be strictly positive')
+    
+            if max_points < y.shape[-1]:
+                target_sr = min(max_sr, (sr * y.shape[-1]) // max_points)
+    
+            hop_length = sr // target_sr
+    
+            if y.ndim == 1:
+                y = util.frame(y, hop_length).max(axis=0)
+            else:
+                y = np.vstack([util.frame(_, hop_length).max(axis=0) for _ in y])
+    
+        if y.ndim > 1:
+            y_top = y[0]
+            y_bottom = -y[1]
+        else:
+            y_top = y
+            y_bottom = -y
+    
+        axes = plt.gca()
+    
+#        kwargs.setdefault('color', next(axes._get_lines.prop_cycler)['color'])
+    
+        locs = offset + core.frames_to_time(np.arange(len(y_top)),
+                                            sr=sr,
+                                            hop_length=hop_length)
+        self.axes.fill_between(locs, y_bottom, y_top)
+    
+        axes.set_xlim([locs.min(), locs.max()])
+        if x_axis == 'time':
+            axes.xaxis.set_major_formatter(display.TimeFormatter(lag=False))
+            axes.xaxis.set_label_text('Time')
+        elif x_axis is None or x_axis in ['off', 'none']:
+            axes.set_xticks([])
+        else:
+            raise ParameterError('Unknown x_axis value: {}'.format(x_axis))
+
             
 #VLC Player conf.          
 instance = vlc.Instance()
--- GUI/mathplotpyqt5.py
+++ GUI/mathplotpyqt5.py
@@ -1,46 +1,122 @@
+# embedding_in_qt5.py --- Simple Qt5 application embedding matplotlib canvases
+#
+# Copyright (C) 2005 Florent Rougon
+#               2006 Darren Dale
+#               2015 Jens H Nielsen
+#
+# This file is an example program for matplotlib. It may be used and
+# modified with no restriction; raw copies as well as modified versions
+# may be distributed without limitation.
+
+from __future__ import unicode_literals
 import sys
+import os
 import random
 import matplotlib
-matplotlib.use("Qt5Agg")
-from PyQt5 import QtCore
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget
+import warnings
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import Formatter, ScalarFormatter
+from matplotlib.ticker import LogLocator, FixedLocator, MaxNLocator
+from matplotlib.ticker import SymmetricalLogLocator
+import librosa
+from librosa import display
+from librosa import core
+from librosa import util
+from librosa.util.exceptions import ParameterError
+# Make sure that we are using QT5
+matplotlib.use('Qt5Agg')
+from PyQt5 import QtCore, QtWidgets
+
 from numpy import arange, sin, pi
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
+progname = os.path.basename(sys.argv[0])
+progversion = "0.1"
+
 
 class MyMplCanvas(FigureCanvas):
     """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
+
+    def __init__(self, parent=None, width=3, height=2, dpi=250):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
-        # We want the axes cleared every time plot() is called
-        self.axes.hold(False)
 
         self.compute_initial_figure()
 
-        #
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
 
         FigureCanvas.setSizePolicy(self,
-                QSizePolicy.Expanding,
-                QSizePolicy.Expanding)
+                                   QtWidgets.QSizePolicy.Expanding,
+                                   QtWidgets.QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
 
     def compute_initial_figure(self):
         pass
 
+
 class MyStaticMplCanvas(MyMplCanvas):
     """Simple canvas with a sine plot."""
-    def compute_initial_figure(self):
-        t = arange(0.0, 3.0, 0.01)
-        s = sin(2*pi*t)
-        self.axes.plot(t, s)
 
+    def compute_initial_figure(self):
+        y, sr = librosa.load(librosa.util.example_audio_file(), duration=10)
+        max_points=5e4
+        x_axis='time'
+        offset=0.0
+        max_sr=1000
+        util.valid_audio(y, mono=False)
+
+        if not (isinstance(max_sr, int) and max_sr > 0):
+            raise ParameterError('max_sr must be a non-negative integer')
+    
+        target_sr = sr
+        hop_length = 1
+    
+        if max_points is not None:
+            if max_points <= 0:
+                raise ParameterError('max_points must be strictly positive')
+    
+            if max_points < y.shape[-1]:
+                target_sr = min(max_sr, (sr * y.shape[-1]) // max_points)
+    
+            hop_length = sr // target_sr
+    
+            if y.ndim == 1:
+                y = util.frame(y, hop_length).max(axis=0)
+            else:
+                y = np.vstack([util.frame(_, hop_length).max(axis=0) for _ in y])
+    
+        if y.ndim > 1:
+            y_top = y[0]
+            y_bottom = -y[1]
+        else:
+            y_top = y
+            y_bottom = -y
+    
+        axes = plt.gca()
+    
+#        kwargs.setdefault('color', next(axes._get_lines.prop_cycler)['color'])
+    
+        locs = offset + core.frames_to_time(np.arange(len(y_top)),
+                                            sr=sr,
+                                            hop_length=hop_length)
+        self.axes.fill_between(locs, y_bottom, y_top)
+    
+        axes.set_xlim([locs.min(), locs.max()])
+        if x_axis == 'time':
+            axes.xaxis.set_major_formatter(display.TimeFormatter(lag=False))
+            axes.xaxis.set_label_text('Time')
+        elif x_axis is None or x_axis in ['off', 'none']:
+            axes.set_xticks([])
+        else:
+            raise ParameterError('Unknown x_axis value: {}'.format(x_axis))
 
 class MyDynamicMplCanvas(MyMplCanvas):
     """A canvas that updates itself every second with a new plot."""
+
     def __init__(self, *args, **kwargs):
         MyMplCanvas.__init__(self, *args, **kwargs)
         timer = QtCore.QTimer(self)
@@ -53,30 +129,31 @@ class MyDynamicMplCanvas(MyMplCanvas):
     def update_figure(self):
         # Build a list of 4 random integers between 0 and 10 (both inclusive)
         l = [random.randint(0, 10) for i in range(4)]
-
+        self.axes.cla()
         self.axes.plot([0, 1, 2, 3], l, 'r')
         self.draw()
 
-class ApplicationWindow(QMainWindow):
+
+class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        QMainWindow.__init__(self)
+        QtWidgets.QMainWindow.__init__(self)
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
         self.setWindowTitle("application main window")
 
-        self.file_menu = QMenu('&File', self)
+        self.file_menu = QtWidgets.QMenu('&File', self)
         self.file_menu.addAction('&Quit', self.fileQuit,
-                QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
+                                 QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
         self.menuBar().addMenu(self.file_menu)
 
-        self.help_menu = QMenu('&Help', self)
+        self.help_menu = QtWidgets.QMenu('&Help', self)
         self.menuBar().addSeparator()
         self.menuBar().addMenu(self.help_menu)
 
         self.help_menu.addAction('&About', self.about)
 
-        self.main_widget = QWidget(self)
+        self.main_widget = QtWidgets.QWidget(self)
 
-        l = QVBoxLayout(self.main_widget)
+        l = QtWidgets.QVBoxLayout(self.main_widget)
         sc = MyStaticMplCanvas(self.main_widget, width=5, height=4, dpi=100)
         dc = MyDynamicMplCanvas(self.main_widget, width=5, height=4, dpi=100)
         l.addWidget(sc)
@@ -94,25 +171,25 @@ class ApplicationWindow(QMainWindow):
         self.fileQuit()
 
     def about(self):
-        QMessageBox.about(self, "About",
-  """embedding_in_qt5.py example
-  Copyright 2015 BoxControL
+        QtWidgets.QMessageBox.about(self, "About",
+                                    """embedding_in_qt5.py example
+Copyright 2005 Florent Rougon, 2006 Darren Dale, 2015 Jens H Nielsen
 
-  This program is a simple example of a Qt5 application embedding matplotlib
-  canvases. It is base on example from matplolib documentation, and initially was
-  developed from Florent Rougon and Darren Dale.
+This program is a simple example of a Qt5 application embedding matplotlib
+canvases.
 
-  http://matplotlib.org/examples/user_interfaces/embedding_in_qt4.html
+It may be used and modified with no restriction; raw copies as well as
+modified versions may be distributed without limitation.
 
-  It may be used and modified with no restriction; raw copies as well as
-  modified versions may be distributed without limitation."""
-  )
+This is modified from the embedding in qt4 example to show the difference
+between qt4 and qt5"""
+                                )
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
 
-    aw = ApplicationWindow()
-    aw.setWindowTitle("PyQt5 Matplot Example")
-    aw.show()
-    #sys.exit(qApp.exec_())
-    app.exec_()
+qApp = QtWidgets.QApplication(sys.argv)
+
+aw = ApplicationWindow()
+aw.setWindowTitle("%s" % progname)
+aw.show()
+sys.exit(qApp.exec_())
+#qApp.exec_()
